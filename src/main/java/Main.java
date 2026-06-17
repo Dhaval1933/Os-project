f09bfdab656359145443e4f18c0b5f5157225f81
Dhaval Mittal <dhavalmittal13@gmail.com>
2026-06-17 12:56:55 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,25 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
+        Scanner sc=new Scanner(System.in);
+        while(true){
             System.out.print("$ ");
-            if (!sc.hasNextLine()) {
+            if(!sc.hasNext()){
                 break;
             }
-            String a = sc.nextLine().trim();
+            String a=sc.nextLine().trim();
             if (a.isEmpty()) {
                 continue;
             }
-            String[] inputParts = a.split(" ");
-            String command = inputParts[0];
-            if (command.equals("echo")) {
-                StringBuilder sb = new StringBuilder();
-                for (int i = 1; i < inputParts.length; i++) {
-                    sb.append(inputParts[i]);
-                    if (i < inputParts.length - 1) {
-                        sb.append(" ");
-                    }
-                }
-                System.out.println(sb.toString());
-            } else {
-                System.out.println(a + ": command not found");
+            if(a.equals("exit")){
+                break;
             }
+            if(a.startsWith("echo ")){
+                System.out.println(a.substring(5));
+            }
+            else
+            System.err.println(a+": command not found");
         }
     }
 }
